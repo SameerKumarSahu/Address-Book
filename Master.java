266df88261a38871fe6f84cf7131d9b40984edbd
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-26 21:19:37 +0530

    Welcome Meassage

---
+++ Master.java
@@ -0,0 +1,13 @@
+package com.AddressBook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Master {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book System...");
+
+	}
+
+}
